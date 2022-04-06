1ed968aaaaa2c1ca2be6a299ecb101d367475520
NguyenVu11796 <hoangnguyen96117@gmail.com>
2022-04-06 17:45:38 +0700

    added three validators in Validations

---
+++ Assignment1/Functionalities/UserInput.java
@@ -0,0 +1,24 @@
+package Assgn1FurtherProg.Assignment1.Functionalities;
+
+import java.util.Scanner;
+import Assgn1FurtherProg.Assignment1.Interfaces.StudentEnrolmentManager;
+
+/**
+ * This class gives the users different options for inputs and data location
+ */
+public class UserInput {
+    //Attributes
+    private static Scanner input;
+
+    //Methods
+    public static String getStudentID(StudentEnrolmentManager sem) {
+        input = new Scanner(System.in);
+        String studentID;
+
+        while (true) {
+            System.out.println("===========================================================================");
+            System.out.print("\nEnter a student ID consisting of 's' and a series of numbers (e.g. s3881101): ");
+
+        }
+    }
+}
---
+++ Assignment1/Functionalities/Validations.java
@@ -0,0 +1,61 @@
+package Assgn1FurtherProg.Assignment1.Functionalities;
+
+import Assgn1FurtherProg.Assignment1.Models.Courses;
+import Assgn1FurtherProg.Assignment1.Models.Students;
+import Assgn1FurtherProg.Assignment1.Models.studentEnrolment;
+
+import java.util.ArrayList;
+
+/**
+ * This class is a collection of validators and condition checkers
+ * To avoid repetition in execution.
+ */
+public class Validations {
+    //Methods
+    // 1) When populating the data or enrolling students in a course in a semester
+
+    /**
+     * Checks if a student is already in the student list of the semester
+     * @param student: a Student object
+     * @param studentsArrayList: a list of Student objects
+     * @return true if a student is already enrolled in the list, false otherwise
+     */
+    public static boolean studentAlreadyEnrolled(Students student, ArrayList<Students> studentsArrayList) {
+        for(Students studentInList: studentsArrayList) {
+            if (studentInList.equals(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if a course is already available in course list.
+     * @param course a Course object
+     * @param courseList a list of Course objects.
+     * @return true if a course is already available in course list, false otherwise.
+     */
+    public static boolean courseAlreadyExists(Courses course, ArrayList<Courses> courseList) {
+        for (Courses courseInList: courseList) {
+            if (courseInList.equals(course)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if an enrolment is already available in enrolment list.
+     * @param enrolment an Enrolment object.
+     * @param enrolmentList a list of Enrolment objects.
+     * @return true if an enrolment is already available in enrolment list, false otherwise.
+     */
+    public static boolean enrolmentAlreadyExists(studentEnrolment enrolment, ArrayList<studentEnrolment> enrolmentList) {
+        for (studentEnrolment sE: enrolmentList) {
+            if (sE.getStudent().equals(enrolment.getStudent()) && sE.getCourse().equals(enrolment.getCourse()) && sE.getSemester().equalsIgnoreCase(enrolment.getSemester())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
